519c87b011e88a819cdd1cf2ac295cdb96a15d81
Sathursha <139601196+Sathursha@users.noreply.github.com>
2024-12-01 09:57:47 +0000

    index file fetch data from table

---
+++ Public/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'dbconf.php';
+try{
+    $sql="SELECT * FROM student";
+
+    $result=mysqli_query($connect,$sql);
+
+    if(mysqli_num_rows($result)>0){
+        while($row=mysqli_fetch_assoc($result)){
+            print_r($row);
+        }
+    }
+    else{
+        echo "No resullts";
+    }
+}
+
+catch(Exception $e){
+    die($e->getMessage());
+}
+?>
