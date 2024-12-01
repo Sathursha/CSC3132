c9a5122bbeaecd1839644d9af1c30ae71eb059fd
Sathursha <139601196+Sathursha@users.noreply.github.com>
2024-12-01 10:46:52 +0000

    add table

--- Public/dbconf.php
+++ Public/dbconf.php
@@ -1,23 +1,22 @@
 <?php
-//constant variable
-define('SERVERNAME', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DBNAME', 'studentdb');
-try {
-	//connect with database
-	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
-	if (!$connect) {
-		die("connection failed".mysqli_connect_error());
-	} 
-	else {
-		//echo "Connected successfully";
-	}
-} 
-catch (Exception $e) {
-	die($e->getMessage());
-}
 
-//echo "abc<br>";
+define('SERVERNAME','127.0.0.1:3306');
+define('USERNAME', 'root');
+define('PASSWORD','mariadb');
+define('DBNAME','school');
+
+try{
+    $connect=mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+    if (!$connect){
+        die("connection failed".mysqli_connect_error());
+    }
+    else{
+        echo "Connected Successfully";
+    }
+}
+catch(Exception $e){
+    die($e->getMessage());
+
+}
 
 ?>
